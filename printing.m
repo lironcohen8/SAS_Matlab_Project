c3852fff7e4264d2d58356abff8f1a8b885e7684
yuvalmor96 <65467427+yuvalmor96@users.noreply.github.com>
2021-06-03 16:46:09 +0300

    some changes

--- printing.m
+++ printing.m
@@ -1,8 +1,40 @@
+%e
 w = linspace(-pi,pi,2*10000+1);
-n = -10000:10000;
+n = -10:10;
 xn = cos((2.*pi./5).*n) + cos(((pi)./5).*n);
-fft_xn = fftshift(fft(xn));
-plot(w,abs(fft_xn));
-title('x[n] DTFT')
-ylabel('X(e^(j\omega))');
-xlabel('\omega [rad/sec]');
+yn2 = cos((2.*pi./5).*n) + cos(((pi)./5).*n);
+yn3 = cos(((pi)./5).*n);
+yn4 = cos(((pi)./5).*n);
+yn6 = 0;
+%h2
+stem(n,abs(yn2));
+hold on
+stem(n, abs(xn));
+hold off
+title('x[n] and y_3[n]')
+xlabel('n [sec]');
+ylabel('y_2n');
+
+%h3
+stem(n,abs(yn3),n, abs(xn));
+hold on;
+plot(n, abs(xn));
+hold off;
+title('x[n] and y_3[n]')
+xlabel('n [sec]');
+ylabel('y_3n');
+
+%h4
+title('x[n] and y4[n]')
+xlabel('n [sec]');
+ylabel('y_4n');
+%h6
+stem(n,abs(yn6));
+hold on;
+plot(n, abs(xn));
+hold off;
+title('x[n] and y_6[n]')
+xlabel('n [sec]');
+ylabel('y_6n');
+
+
