4b07f59c4c4bbb05b699ba50882d2ccc9c1d9ec0
yuvalmor96 <65467427+yuvalmor96@users.noreply.github.com>
2021-06-05 15:23:23 +0300

    added screenshots

--- matlabC.m
+++ matlabC.m
@@ -1,17 +1,17 @@
 %a
-t = -10000:10000;
+t = -100:100;
 x1 = sinc(t./6);
 x2 = cos(pi.*t./12)+sin(pi.*t./6);
 fft_x1= fftshift(fft(x1));
 fft_x2= fftshift(fft(x2));
 plot(t,abs(fft_x1));
-title('fft x1 - Part c')
-ylabel('X(j\Omega)');
+title('X_1 - FFT')
+ylabel('|X(j\Omega)|');
 xlabel('\Omega [rad/sec]');
 
 plot(t,abs(fft_x2));
-title('fft x2 - Part c')
-ylabel('X(j\Omega)');
+title('X_2 - FFT')
+ylabel('|X(j\Omega)|');
 xlabel('\Omega [rad/sec]');
 
 %d
@@ -19,15 +19,15 @@ n = -50:50;
 xn1 =  sinc(n./3);
 fft_xn1= fftshift(fft(xn1));
 stem(n,abs(fft_xn1));
-title('fft xn1 - Part c')
-ylabel('X(e.^j\Omega)');
+title('xn_1 - FFT ')
+ylabel('|X(e.^{j\Omega})|');
 xlabel('n [sec]');
 
 xn2 = cos(pi.*n./6)+sin(pi.*n./3);
 fft_xn2= fftshift(fft(xn2));
 stem(n,abs(fft_xn2));
-title('fft xn2 - Part c')
-ylabel('X(e.^j\Omega)');
+title('xn_2 - FFT')
+ylabel('X(e.^{j\Omega})');
 xlabel('n [sec]');
 
 clear;
--- printing.m
+++ printing.m
@@ -1,5 +1,12 @@
-numerator = [32,-24,18,-9];
-denomanator = [32,-40,12,0];
-ts = 1;
-sys = tf(numerator,denomanator,ts);
-pzmap(sys)
+t = -100:100;
+x1 = sinc(t./6);
+x2 = cos(pi.*t./12)+sin(pi.*t./6);
+fft_x1= fftshift(fft(x1));
+fft_x2= fftshift(fft(x2));
+n = -50:50;
+xn2 = cos(pi.*n./6)+sin(pi.*n./3);
+fft_xn2= fftshift(fft(xn2));
+stem(n,abs(fft_xn2));
+title('xn_2 - FFT')
+ylabel('|X(e.^{j\Omega})|');
+xlabel('n [sec]');
