f8ea385cccb536cdc7e7c9a83d08046b9f34a1a1
yuvalmor96 <65467427+yuvalmor96@users.noreply.github.com>
2021-06-09 22:36:11 +0300

    almost done

--- matlabC.m
+++ matlabC.m
@@ -47,8 +47,8 @@ xlabel('n [sec]');
 T=2;
 N=10;
 L=T*N;
-n = -100:100;
-t = linspace(-100,100,length(xn1)*L);
+n = -50:50;
+t = linspace(-50,50,length(xn1)*L);
 xn1 =  sinc(T.*n./6);
 for i=1:length(xn1)-1
     for j=1:L
@@ -68,9 +68,9 @@ xlabel('n [sec]');
 T=2;
 N=10;
 L=T*N;
-n = -100:100;
+n = -50:50;
 xn2 = cos(T.*pi.*n./12)+sin(T.*pi.*n./6);
-t = linspace(-100,100,length(xn2)*L);
+t = linspace(-50,50,length(xn2)*L);
 for i=1:length(xn2)-1
     for j=1:L
         zoh(i*L+j) = xn2(i);
@@ -85,14 +85,14 @@ title('xn_2 - ZOH ')
 ylabel('|xn_2|');
 xlabel('n [sec]');
 
-%e- xn1 foh
 
+%e- xn1 foh
 T=2;
 N=10;
 L=T*N;
 n = -100:100;
-t = linspace(-100,100,length(xn1)*L);
 xn1 =  sinc(T.*n./6);
+t = linspace(-100,100,length(xn1)*L);
 for i=1:length(xn1)-1
     for j=1:L
         foh(i*L+j) = xn1(i)+(j/L)*(xn1(i+1)-xn1(i))
@@ -107,7 +107,26 @@ title('xn_1 - FOH ')
 ylabel('|xn_1|');
 xlabel('n [sec]');
 
-
+%e- xn2 foh
+T=2;
+N=10;
+L=T*N;
+n = -100:100;
+xn2 = cos(T.*pi.*n./12)+sin(T.*pi.*n./6);
+t = linspace(-100,100,length(xn2)*L);
+for i=1:length(xn2)-1
+    for j=1:L
+        foh(i*L+j) = xn2(i)+(j/L)*(xn2(i+1)-xn2(i))
+    end;
+end;   
+figure;
+plot(t,foh);
+hold on;
+plot(n,xn2);
+hold off;
+title('xn_2 - FOH ')
+ylabel('|xn_2|');
+xlabel('n [sec]');
 
 
 
