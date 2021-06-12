6f2dbadd9e77560a62b6d541880b1acefaba0196
yuvalmor96 <65467427+yuvalmor96@users.noreply.github.com>
2021-06-12 17:43:47 +0300

    added some screenshots

--- printing2.m
+++ printing2.m
@@ -1,10 +1,23 @@
-%F - pole zero map
-sympref('HeavisideAtOrigin', 1);
-h = (((0.5).^n.*heaviside(n)) + ((0.75).^n.*heaviside(n-2)));
-H = ztrans(h);
-[numerator, denominator] = numden(H);
-numerator_c = sym2poly(numerator);
-denominator_c = sym2poly(denominator);
-ts = 1;
-sys = tf(numerator_c,denominator_c,ts);
-pzmap(sys)
+%e- xn2 ideal
+T=2;
+N=10;
+n = -50:50;
+xn2 = cos(T.*pi.*n./12)+sin(T.*pi.*n./6);
+t = linspace(-50,50,length(xn2));
+for i=1:length(xn2)
+    if mod(i,T) == 0
+        xs(i) = xn2(i);
+    else
+        xs(i) = 0;
+    end
+end 
+ideal = conv(xs, sinc(t./T), 'same');
+figure;
+plot(t,ideal);
+hold on;
+plot(n,xn2);
+hold off;
+title('xn_1 - Ideal');
+ylabel('xn_2');
+xlabel('n[sec]');
+
