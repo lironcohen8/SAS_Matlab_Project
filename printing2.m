26d87287f0fbeb0ca13c56658d75176b3d5cd3e9
yuvalmor96 <65467427+yuvalmor96@users.noreply.github.com>
2021-06-12 12:50:04 +0300

    finished part B

--- printing2.m
+++ printing2.m
@@ -1,11 +1,53 @@
-n = linspace(-50,50,100);
-T=2;
-w = linspace(-pi,pi,100);
-x2 = cos(pi.*t./12)+sin(pi.*t./6);
-xn2 = cos(T.*pi.*n./12)+sin(T.*pi.*n./6);
-fft_xn2= fftshift(fft(xn2));
-figure;
-stem(w,abs(fft_xn2));
-title('Xn_2(e^{j\omega})')
-ylabel('|X(e^{j\omega})|');
-xlabel('\omega[rad/sec]');
+load('LPF.mat');
+%e
+n = -50:50;
+xn = cos((2.*pi./5).*n) + cos(((pi)./5).*n);
+%h2
+yn2 = conv(xn,h2);
+subplot(2,2,1);
+plot(n,yn2(1:101));
+hold on;
+plot(n, xn);
+hold off;
+title('x[n] and y_2[n]');
+xlabel('n [sec]');
+ylabel('x[n] and y_2[n]');
+axis([0 50 -1.5 2.5]);
+
+%h3
+yn3 = conv(xn,h3);
+subplot(2,2,2);
+plot(n,yn3(1:101));
+hold on;
+plot(n, xn);
+hold off;
+title('x[n] and y_3[n]');
+xlabel('n [sec]');
+ylabel('x[n] and y_3[n]');
+axis([0 50 -1.5 2.5]);
+
+%h4
+yn4 = conv(xn,h4);
+subplot(2,2,3);
+plot(n,yn4(1:101));
+hold on;
+plot(n, xn);
+hold off;
+title('x[n] and y_4[n]');
+xlabel('n [sec]');
+ylabel('x[n] and y_4[n]');
+axis([0 50 -1.5 2.5]);
+
+%h6
+yn6 = conv(xn,h6);
+subplot(2,2,4);
+plot(n,yn6(1:101));
+hold on;
+plot(n, xn);
+hold off;
+title('x[n] and y_6[n]');
+xlabel('n [sec]');
+ylabel('x[n] and y_6[n]');
+axis([0 50 -1.5 2.5]);
+
+
